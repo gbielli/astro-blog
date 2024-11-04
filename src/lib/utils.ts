b792c2824ad1124d75b82f3c18876f7deaa88937
Guillaume Bielli <guillaume@MacBook-Pro-de-IT.local>
2024-11-04 21:52:39 +0100

    utils

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -5,12 +5,21 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
-export function slugify(str: string) {
-  return str
-    .toLowerCase()
-    .normalize("NFD") // Décompose les caractères accentués
-    .replace(/[\u0300-\u036f]/g, "") // Supprime les accents
-    .replace(/[^a-z0-9]+/g, "-") // Remplace les caractères spéciaux par des tirets
-    .replace(/^-+|-+$/g, "") // Supprime les tirets au début et à la fin
-    .trim(); // Enlève les espaces
+export function slugify(text: string | undefined): string {
+  // Gérer le cas où le texte est undefined ou null
+  if (!text) {
+    return "";
+  }
+
+  return text
+    .toString() // Convertir en string
+    .normalize("NFD") // Normaliser les caractères Unicode
+    .replace(/[\u0300-\u036f]/g, "") // Retirer les accents
+    .toLowerCase() // Convertir en minuscules
+    .trim() // Retirer les espaces au début et à la fin
+    .replace(/\s+/g, "-") // Remplacer les espaces par des tirets
+    .replace(/[^\w-]+/g, "") // Retirer les caractères non-word
+    .replace(/--+/g, "-") // Remplacer les multiples tirets par un seul
+    .replace(/^-+/, "") // Retirer les tirets au début
+    .replace(/-+$/, ""); // Retirer les tirets à la fin
 }
